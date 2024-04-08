452a7c6c0e1639a8a45c65c2c11ba16a31219985
wangyifan <ivan_sir@163.com>
2024-04-08 14:53:03 +0800

    design-patterns.txt

--- design-pattern/src/main/java/com/ivan/creator/singleton/Singleton4.java
+++ design-pattern/src/main/java/com/ivan/creator/singleton/Singleton4.java
@@ -1,5 +1,17 @@
 package com.ivan.creator.singleton;
 
 public class Singleton4 {
+    private Singleton4() {
+    }
 
+    /**
+     * 内部类， jvm 在加载外部类的时候不会加载内部类 只有在内部类的方法/属性 被调用才会被加载。
+     */
+    private static class SingletonHolder {
+        private static final Singleton4 INSTANCE = new Singleton4();
+    }
+
+    public static Singleton4 getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
 }
---
+++ design-pattern/src/main/java/com/ivan/creator/singleton/Singleton5.java
@@ -0,0 +1,5 @@
+package com.ivan.creator.singleton;
+
+public enum Singleton5 {
+    INSTANCE;
+}
