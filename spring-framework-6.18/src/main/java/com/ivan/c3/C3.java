ed2bb5c1a016545734a96846c91b7461b0282787
wangyifan <ivan_sir@163.com>
2025-01-03 17:52:18 +0800

    spring-core 2025-01

--- spring-framework-6.18/src/main/java/com/ivan/c3/C3.java
+++ spring-framework-6.18/src/main/java/com/ivan/c3/C3.java
@@ -1,16 +1,34 @@
 package com.ivan.c3;
 
-import org.gradle.internal.impldep.org.apache.sshd.client.auth.pubkey.UserAuthPublicKeyIterator;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
+
+import java.util.Map;
+
 
 public class C3 {
 
     public static void main(String[] args) {
 //        test1();
 //        test2();
-        test3();
+//        test3();
+//        test4();
+        test5();
+    }
+
+    private static void test5() {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("file:D:\\ivan-project\\Practice\\spring-framework-6.18\\src\\main\\java\\com\\ivan\\c3\\c3.xml");
+        System.out.println(context.getBean("autowiringModeBean1",AutowiringModeBean1.class).auto);
+    }
+
+    private static void test4() {
+        GenericApplicationContext context = new GenericApplicationContext();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(context);
+        reader.loadBeanDefinitions("file:D:\\ivan-project\\Practice\\spring-framework-6.18\\src\\main\\java\\com\\ivan\\c3\\c3.xml");
+        context.refresh();
+        System.out.println("container initialized...");
+        System.out.println(context.getBean("lazyBean"));
     }
 
     private static void test3()  {
@@ -92,3 +110,34 @@ class MyDataSource{
         this.password = password;
     }
 }
+
+class LazyBean{
+    public LazyBean(){
+        System.out.println("lazy bean init");
+    }
+}
+class NonLazyBean{
+    public NonLazyBean(){
+        System.out.println("non lazy bean init");
+    }
+}
+
+//TODO 注入集合对象怎么注入
+
+/**
+ * Autowiring Collaborators
+ */
+class AutowiringModeBean1{
+    public AutowiredBean auto;
+
+    public Map<String, AutowiredBean> map;
+
+    public void setMap(Map<String, AutowiredBean> map) {
+        this.map = map;
+    }
+
+    public void setAuto(AutowiredBean auto) {
+        this.auto = auto;
+    }
+}
+class AutowiredBean{}
