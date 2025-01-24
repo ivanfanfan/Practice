428a2924ab24d9db6f72a52f0f9a4692d97bc2bd
wangyifan <ivan_sir@163.com>
2025-01-24 09:51:44 +0800

    spring security build

--- spring-security/src/main/java/com/ivan/SpringSecurityApplication.java
+++ spring-security/src/main/java/com/ivan/SpringSecurityApplication.java
@@ -2,7 +2,32 @@ package com.ivan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
+import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
@@ -10,7 +35,90 @@ public class SpringSecurityApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityApplication.class, args);
     }
+}
 
+@RestController
+class TestController {
+
+    @GetMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+
+
+    //build securityContextHolder
+    public void buildSecurityContextHolder() {
+        // build security context
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        // build authentication for security context;
+        Authentication authentication = new TestingAuthenticationToken("username", "password", "ROLE_USER");
+        context.setAuthentication(authentication);
+        // set security context to security context holder
+        SecurityContextHolder.setContext(context);
+    }
+
+    public void getInformationOfAuthentication() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        //get name
+        String name = authentication.getName();
+        // get principal
+        Object principal = authentication.getPrincipal();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+    }
 }
 
 
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(new Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry>() {
+                    @Override
+                    public void customize(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authorizationManagerRequestMatcherRegistry) {
+                        authorizationManagerRequestMatcherRegistry.anyRequest().authenticated();
+                    }
+                })
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+}
+
+
+//
+//@EnableWebSecurity
+//@Configuration
+class DefaultSecurityConfig {
+    @Bean
+    @ConditionalOnMissingBean(UserDetailsService.class)
+    InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("bcrypt", null);
+        passwordEncoder.setDefaultPasswordEncoderForMatches(new BCryptPasswordEncoder(16));
+
+        String generatedPassword = passwordEncoder.encode("password");
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user")
+                        .password(generatedPassword)
+                        .roles("USER").build());
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(AuthenticationEventPublisher.class)
+    DefaultAuthenticationEventPublisher defaultAuthenticationEventPublisher(ApplicationEventPublisher delegate) {
+        return new DefaultAuthenticationEventPublisher(delegate);
+    }
+}
--- spring-security/src/test/java/com/ivan/SpringSecurityApplicationTests.java
+++ spring-security/src/test/java/com/ivan/SpringSecurityApplicationTests.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 @SpringBootTest
 class SpringSecurityApplicationTests {
@@ -17,6 +21,16 @@ class SpringSecurityApplicationTests {
                 .build();
         System.out.println(user.getPassword());
 // {bcrypt}$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG
+    }
+
+    @Test
+    void test() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
+        String result = encoder.encode("myPassword");
+//        System.out.println(result);
+        String myPassword = encoder.encode("myPassword");
+        System.out.println(myPassword);
+        assertTrue(encoder.matches("myPassword", result));
     }
 
 }
