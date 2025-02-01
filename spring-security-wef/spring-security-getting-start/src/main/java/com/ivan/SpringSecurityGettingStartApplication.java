4366246680a4e25a2cc968cd4c8fd5ee948f5092
wangerfan <ivansir@163.com>
2025-02-01 14:24:40 +0800

    spring security and word

---
+++ spring-security-wef/spring-security-getting-start/src/main/java/com/ivan/SpringSecurityGettingStartApplication.java
@@ -0,0 +1,13 @@
+package com.ivan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityGettingStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityGettingStartApplication.class, args);
+    }
+
+}
