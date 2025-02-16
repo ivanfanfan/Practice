cf4a8a12bdba0abd5b069b3f9adf99d61bf4c8ea
wangyifan <ivan_sir@163.com>
2025-02-16 21:05:42 +0800

    validate databind

--- java-io/src/main/java/com/ivan/character/CopyLines.java
+++ java-io/src/main/java/com/ivan/character/CopyLines.java
@@ -1,9 +1,47 @@
 package com.ivan.character;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class CopyLines {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+//        test1();
+        test2();
+    }
+
+    private static void test2() throws Exception {
+        BufferedReader inputStream = new BufferedReader(
+                new FileReader("E:\\code\\Practice\\spring security.txt")
+        );
+
+        PrintWriter outputStream = new PrintWriter(
+                new FileWriter("E:\\code\\Practice\\spring security ordered.txt")
+        );
+        String word;
+        List<String> words = new ArrayList<>();
+//        words.add(word);
+
+        while((word = inputStream.readLine()) != null) {
+//            outputStream.println(word);
+            words.add(word);
+        }
+        words.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+        for (int i  = 0; i < words.size(); i++) {
+            outputStream.println(words.get(i));
+        }
+
+        inputStream.close();
+        outputStream.close();
+    }
+
+    private static void test1() throws Exception {
         BufferedReader inputStream = null;
         PrintWriter outputStream = null;
 
@@ -23,5 +61,10 @@ public class CopyLines {
                 outputStream.close();
             }
         }
+
     }
+
+
+
+
 }
